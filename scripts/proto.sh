f15a13d4e5250ffef8f3eeb8ccfd361fc4e09bc4
David Brown <davidb@netflix.com>
2020-06-18 21:20:33 -0500

    go output

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -15,6 +15,12 @@ TARGET=indigo_protobuf  # where to generate the python files
 # =====
 
 # =====
+export GO111MODULE=on
+which -s protoc-gen-go || go get github.com/golang/protobuf/protoc-gen-go@v1.3
+export PATH="$PATH:$(go env GOPATH)/bin"
+# =====
+
+# =====
 # work
 TMPDIR=$(mktemp -d)
 mkdir -p "${TMPDIR}/${TARGET}"
@@ -23,6 +29,9 @@ python -m grpc_tools.protoc --python_out=. --grpc_python_out=. -I "${TMPDIR}" "$
 python -m grpc_tools.protoc --python_out=. --grpc_python_out=. -I "${TMPDIR}" "${TMPDIR}/${TARGET}/indigo_influx.proto"
 protoc -I . --python_betterproto_out=indigo_protobuf_betterproto proto/indigo.proto
 protoc -I . --python_betterproto_out=indigo_protobuf_betterproto proto/indigo_influx.proto
+protoc -I . proto/indigo.proto --go_out=plugins=grpc:.
+protoc -I . proto/indigo_influx.proto --go_out=plugins=grpc:.
+
 
 rm -rf "${TMPDIR}"
 # =====
